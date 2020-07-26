9bb8a1599b889849eede475c8e03883883087e9c
Oshinka <oshinka94@gmail.com>
2020-07-26 21:35:13 +0530

    Updates

---
+++ precache-manifest.92e99b5e7ae4252d9d8746653247b2cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d7eb84b2945b11feadf7f56f219aee4",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "a49bd9cd0c0269cc2a7d",
+    "url": "/my-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/my-app/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a49bd9cd0c0269cc2a7d",
+    "url": "/my-app/static/js/main.e8837a6b.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
